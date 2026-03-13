6ece4785cb150823f0cb56ab32535a3ffb58eab6
Martim Leme <up202304944@up.pt>
2026-03-13 23:09:49 +0000

    first commit. created first version of the menu

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+     
+    int op; 
+
+    while (op != 0) {
+        
+        cout << "1 - Load input file\n";
+        cout << "2 - Show submissions\n";
+        cout << "3 - Show reviewers\n";
+        cout << "4 - Load input file\n";
+        cout << "5 - Show submissions\n";
+        cout << "6 - Show reviewers\n";
+        cout << "7 - Load input file\n";
+        cout << "8 - Show submissions\n";
+        cout << "0 - Exit\n";
+        cout << "Operation: ";
+        cin >> op;
+
+        
+    }
+
+    return 0; 
+}
