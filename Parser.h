37fe4fc4ab4425837cc3f29926b63917d8c3faca
JoaoLeppanen <psyob@gmail.com>
2026-03-15 15:42:15 +0000

    Parser files seem done

--- Parser.h
+++ Parser.h
@@ -1,1 +1,25 @@
-//Parser cria os vertex com a info do ficheiro
+#ifndef PARSER_H
+#define PARSER_H
+
+class CentralManager;
+
+#include <string>
+
+class Parser {
+    private:
+    CentralManager& manager;
+    static void trim(std::string& str);
+
+    void parseSubmissionLine(const std::string& line);//manda info ao manager para instanciar vertices Submission
+    void parseReviewerLine(const std::string& line);//manda info ao manager para instanciar vertices Reviewers
+    void parseParameterLine(const std::string& line);//(Pesos das edges da source para submissions e peso das edges dos reviewers para sink).
+    void parseControlLine(const std::string& line);//guarda Control Parameters
+
+    public:
+    explicit Parser(CentralManager& m) : manager(m) {}
+
+    //Abre o ficheiro e gere leitura
+    bool loadData(const std::string& filename);
+};
+
+#endif
